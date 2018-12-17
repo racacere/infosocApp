f023b1ada77acccfd6fd5c3abaeaae113d6eccf4
Rodrigo Antonio Cáceres Vicencio <racacere@alumnos.inf.utfsm.cl>
2018-12-17 00:56:18 -0300

    Added conceptual images

--- src/data/data.tematics.ts
+++ src/data/data.tematics.ts
@@ -1,30 +1,64 @@
 export const CATEGORIES = [
     {
-        name: "Navega seguro",
+        name: "Correo Seguro",
+        image: "https://diligent.com/wp-content/uploads/2017/07/Are-There-Any-Safe-and-Secure-Alternatives-to-Business-Email.jpg",
         description: "Pequeña descripción",
         subCategories: [
             {
-                name: "Pon atención a la URL",
-                body: "Lorem ipsum dolor sit amet consectetur adipiscing elit vitae turpis himenaeos mollis, vel habitasse arcu sociis ac imperdiet metus maecenas eros netus a quis, augue nullam ut platea porttitor sodales semper laoreet inceptos erat. Magnis donec diam leo ad vivamus fusce placerat ligula imperdiet, sodales in tincidunt euismod libero dictum cursus sollicitudin inceptos, torquent pulvinar primis senectus venenatis vehicula vulputate turpis."
+                name: "Phishing",
+                image: "https://blog.malwarebytes.com/wp-content/uploads/2018/09/shutterstock_749866270-900x506.jpg",
+                body: "El termino Phishing es utilizado para referirse a uno de los métodos mas utilizados por delincuentes cibernéticos para estafar y obtener información confidencial de forma fraudulenta como puede ser una contraseña o información detallada sobre tarjetas de crédito u otra información bancaria de la victima. El estafador, conocido como phisher, se vale de técnicas de ingeniería social, haciéndose pasar por una persona o empresa de confianza en una aparente comunicación oficial electrónica, por lo general un correo electrónico, o algún sistema de mensajería instantánea, redes sociales SMS/MMS, a raíz de un malware o incluso utilizando también llamadas telefónicas.                "
             },
             {
-                name: "Verifica que tenga https",
-                body: "Lorem ipsum dolor sit amet consectetur adipiscing elit vitae turpis himenaeos mollis, vel habitasse arcu sociis ac imperdiet metus maecenas eros netus a quis, augue nullam ut platea porttitor sodales semper laoreet inceptos erat. Magnis donec diam leo ad vivamus fusce placerat ligula imperdiet, sodales in tincidunt euismod libero dictum cursus sollicitudin inceptos, torquent pulvinar primis senectus venenatis vehicula vulputate turpis."
+                name: "Revisa el correo de origen",
+                image: "https://img1.wsimg.com/Sitecore/4/9/illu-professional-email-business-email.png",
+                body: ""
+            },
+            {
+                name: "Estafas Comunes",
+                image: "https://www.veracode.com/sites/default/files/styles/blog_post_resize_960/public/wikileaks-bug-bounty-blackmail.png?itok=UbP1vdHD",
+                body: ""
             },
 
         ]
     },
     {
-        name: "Transferencias Bancarias",
+        name: "Navega Seguro",
+        image: "https://static.makeuseof.com/wp-content/uploads/2016/02/best-browser-670x335.jpg",
         description: "Pequeña descripción",
         subCategories: [
             {
-                name: "Consejos básicos de seguridad",
-                body: "Lorem ipsum dolor sit amet consectetur adipiscing elit vitae turpis himenaeos mollis, vel habitasse arcu sociis ac imperdiet metus maecenas eros netus a quis, augue nullam ut platea porttitor sodales semper laoreet inceptos erat. Magnis donec diam leo ad vivamus fusce placerat ligula imperdiet, sodales in tincidunt euismod libero dictum cursus sollicitudin inceptos, torquent pulvinar primis senectus venenatis vehicula vulputate turpis."
+                name: "Https",
+                image: "https://strongarm.io/wp-content/uploads/2017/11/HTTPS_icon.png",
+                body: ""
             },
             {
-                name: "No des tu contraseña en un correo",
-                body: "Lorem ipsum dolor sit amet consectetur adipiscing elit vitae turpis himenaeos mollis, vel habitasse arcu sociis ac imperdiet metus maecenas eros netus a quis, augue nullam ut platea porttitor sodales semper laoreet inceptos erat. Magnis donec diam leo ad vivamus fusce placerat ligula imperdiet, sodales in tincidunt euismod libero dictum cursus sollicitudin inceptos, torquent pulvinar primis senectus venenatis vehicula vulputate turpis."
+                name: "Fijate en la URL de origen",
+                image: "https://i.blogs.es/dcc721/url1/450_1000.jpg",
+                body: ""
+            },
+
+        ]
+    },
+    {
+        name: "Compras Seguras",
+        image: "https://www.glenmorevisioncenter.com/wp-content/uploads/2018/03/Online-Shopping.jpg",
+        description: "Pequeña descripción",
+        subCategories: [
+            {
+                name: "Responsabilidades legales de las empresas",
+                image: "https://www.sdworx.co.uk/Images/Blog/rulesandregs_330_188.png",
+                body: ""
+            },
+            {
+                name: "Como funciona WebPay",
+                image: "https://www.transbank.cl/web/images/1476460516864?blobnocache=true",
+                body: ""
+            },
+            {
+                name: "Protege tus datos",
+                image: "https://www.eukhost.com/kb/wp-content/uploads/2015/11/800-how-to-change-the-root-user-password-via-whm-1.jpg",
+                body: ""
             },
 
         ]
